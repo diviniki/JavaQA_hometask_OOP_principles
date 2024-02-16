9e2a9d815f6aa52b056b6703041be4f712e9a640
dmitriyn <d.nikitin.rf@gmail.com>
2024-02-16 17:00:35 +0500

    init commit

---
+++ src/main/java/org/somethings/FormDate.java
@@ -0,0 +1,7 @@
+package org.somethings;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/org/somethings/Post.java
@@ -0,0 +1,11 @@
+package org.somethings;
+
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+}
